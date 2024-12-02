b65b2f0266968633b2c721b4b9c956b171ad1035
wopigjwoigjweroi <agizekel@gmail.com>
2024-12-01 21:56:14 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,9 +37,48 @@ int main() {
 
     do {
 
-        
+        interface(); 
+        cin >> select; 
 
-    }
+        switch(select) {
+
+            case 1: 
+                cout << "Enter string to insert: "; 
+                cin >> val; 
+                tree.insertNode(val); 
+                break; 
+
+            case 2: 
+                cout << "Enter string to search: "; 
+                cin >> val; 
+                if (tree.searchNode(val)) {
+
+                    cout << val << " found.\n"; 
+                } else {
+
+                    cout << val << " not found.\n"; 
+                }
+                break; 
+
+            case 3: 
+                cout << "Enter a string to remove: "; 
+                cin >> val; 
+                tree.remove(val); 
+                break; 
+
+            case 4: 
+                cout << "BST Contents In-Order: \n"; 
+                tree.displayInOrder();
+                break; 
+
+            case 5: 
+                cout << "Quitting..."; 
+                break;
+
+            default:
+                cout << "Invalid.\n"; 
+        }
+    } while (select !=5); 
 
     return 0;
 }
