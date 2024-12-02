9fd389e043b7fa44f163be44080cb61fd1a229cb
wopigjwoigjweroi <agizekel@gmail.com>
2024-12-01 20:10:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include "IntBinaryTree.h"
 using namespace std;
 
+void IntBinaryTree::insert(TreeNode*& nodePtr, TreeNode*& newNode) {
+    if (!nodePtr)
+        nodePtr = newNode; 
+    else if (newNode->value < nodePtr->value)
+        insert(nodePtr->left, newNode); 
+    else
+        insert(nodePtr->right, newNode); 
+}
+
+void IntBinaryTree::insertNode(const string& str) {
+    TreeNode* newNode = new TreeNode;
+    newNode->value = str;
+    newNode->left = newNode->right = nullptr;
+    insert(root, newNode);
+}
+
 int main() {
     
 
